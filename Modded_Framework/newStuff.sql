3e8d25fc3bc2f7a559ef4e29a2136b69250890f1
Dom <DomT602@users.noreply.github.com>
2019-01-26 21:07:39 +0000

    For the love of SQL

--- Modded_Framework/newStuff.sql
+++ Modded_Framework/newStuff.sql
@@ -6,31 +6,31 @@ USE `DT_DB`;
 
 CREATE TABLE IF NOT EXISTS `players` (
 	`uid` int(6) NOT NULL AUTO_INCREMENT,
-	`name` varchar(32) NOT NULL,
+	`name` varchar(64) NOT NULL,
 	`pid` varchar(17) NOT NULL,
 	`cash` int(100) NOT NULL DEFAULT '0',
 	`bank` int(100) NOT NULL DEFAULT '50000',
-	`dojlevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
-	`coplevel` enum('0','1','2','3','4','5','6','7') NOT NULL DEFAULT '0',
-	`mediclevel` enum('0','1','2','3','4','5','6','7') NOT NULL DEFAULT '0',
-	`cop_department` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
-	`medic_department` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
+	`dojLevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
+	`copLevel` enum('0','1','2','3','4','5','6','7') NOT NULL DEFAULT '0',
+	`medLevel` enum('0','1','2','3','4','5','6','7') NOT NULL DEFAULT '0',
+	`copDepartment` enum('0','1','2','3','4') NOT NULL DEFAULT '0',
+	`medicDepartment` enum('0','1','2','3','4') NOT NULL DEFAULT '0',
 	`licenses` text NOT NULL DEFAULT '[]',
-	`civ_gear` text NOT NULL DEFAULT '[]',
-	`cop_gear` text NOT NULL DEFAULT '[]',
-	`med_gear` text NOT NULL DEFAULT '[]',
-	`stats` varchar(48) NOT NULL DEFAULT '[100,100,100,5000,0,0,0,0]', /*[hunger,thirst,battery,blood,head,torso,arms,legs]*/
-	`jail_details` text NOT NULL DEFAULT '[0,"",0,""]',
-	`donorlevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
+	`civGear` text NOT NULL DEFAULT '[]',
+	`copGear` text NOT NULL DEFAULT '[]',
+	`medGear` text NOT NULL DEFAULT '[]',
+	`stats` varchar(48) NOT NULL DEFAULT '[100,100,100,5000,[0,0,0,0,0,0]]', /*[hunger,thirst,battery,blood,[head,torso,arm_l,arm_r,leg_l,leg_r]]*/
+	`jailStats` text NOT NULL DEFAULT '[false,"",0,""]',
+	`donorLevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
 	`alive` tinyint(1) NOT NULL DEFAULT '0',
 	`position` varchar(64) NOT NULL DEFAULT '[0,0,0]',
-	`phone_number` varchar(12) NOT NULL,
-	`phone_settings` varchar(150) NOT NULL DEFAULT '[1,"",""]', /*[sound mode,background_path,ringtone_sound]*/
-	`phone_contacts` varchar(150) NOT NULL DEFAULT '[]', /*[number,name]*/
+	`phoneNumber` varchar(12) NOT NULL,
+	`phoneSettings` varchar(150) NOT NULL DEFAULT '[true,"",""]', /*[sound mode,background_path,ringtone_sound]*/
+	`phoneContacts` varchar(900) NOT NULL DEFAULT '[]', /*[[number,name,notes]]*/
 	`skills` varchar(100) NOT NULL DEFAULT '[0,0,0,0,0]',
 	PRIMARY KEY (`uid`),
 	UNIQUE KEY `pid` (`pid`),
-	UNIQUE KEY `phone_number` (`phone_number`)
+	UNIQUE KEY `phoneNumber` (`phoneNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `vehicles` (
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
 	`pid` varchar(17) NOT NULL,
 	`active` tinyint(1) NOT NULL DEFAULT '1',
 	`plate` varchar(7) NOT NULL,
-	`colour` int(3) NOT NULL,
+	`customisation` varchar(256) NOT NULL DEFAULT '[]', /*[tint,wheel colour,spoiler]*/
 	`fuel` double NOT NULL DEFAULT '1',
 	`damage` varchar(256) NOT NULL DEFAULT '[]',
 	`impounded` tinyint(1) NOT NULL DEFAULT '0',
@@ -55,14 +55,14 @@ CREATE TABLE IF NOT EXISTS `houses` (
 	`pos` varchar(64) DEFAULT NULL,
 	`type` tinyint(1) NOT NULL DEFAULT '0',
 	`furniture` text NOT NULL DEFAULT '[]', /*[[id,class,pos,gear,dir],[]]*/
-	`shared` varchar(300) NOT NULL DEFAULT '[]',
-	`alarm` varchar(300) NOT NULL DEFAULT '[false,false,[]]',
+	`shared` varchar(400) NOT NULL DEFAULT '[]',
+	`alarm` varchar(17) NOT NULL DEFAULT '[false,false]',
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `shops` (
 	`id` int(6) NOT NULL AUTO_INCREMENT,
-	`company_name` varchar(48) DEFAULT NULL,
+	`companyName` varchar(48) DEFAULT NULL,
 	`pos` varchar(64) DEFAULT NULL,
 	`items` text NOT NULL DEFAULT '[]', /*[[item,price]]*/
 	`funds` int(100) NOT NULL DEFAULT '0',
@@ -73,12 +73,12 @@ CREATE TABLE IF NOT EXISTS `shops` (
 CREATE TABLE IF NOT EXISTS `companies` (
 	`id` int(6) NOT NULL AUTO_INCREMENT,
 	`owner` varchar(32) NOT NULL,
-	`owner_name` varchar(32) NOT NULL,
+	`ownerName` varchar(32) NOT NULL,
 	`name` varchar(48) NOT NULL,
-	`description` text  NOT NULL,
+	`description` varchar(256)  NOT NULL DEFAULT '',
+	`tasks` varchar(256) NOT NULL DEFAULT '',
 	`bank` int(100) NOT NULL DEFAULT '0',
 	`employees` text,
-	`active` tinyint(1) DEFAULT '1',
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `owner` (`owner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -90,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `pd_db_arrests` (
 	`officer` varchar(64) NOT NULL,
 	`time` int(4) NOT NULL,
 	`bail` int(9) NOT NULL,
-	`insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`insertTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `pd_db_tickets` (
 	`crimes` text NOT NULL,
 	`officer` varchar(64) NOT NULL,
 	`cost` int(7) NOT NULL,
-	`insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`insertTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -111,10 +111,10 @@ CREATE TABLE IF NOT EXISTS `pd_db_warrants` (
 	`officer` varchar(64) NOT NULL,
 	`notes` text NOT NULL,
 	`type` tinyint(1) DEFAULT '0',
-	`approved` tinyint(1) DEFAULT '0',
-	`active` tinyint(1) DEFAULT '0',
-	`completed_by` varchar(64),
-	`insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`approved` varchar(64) DEFAULT '',
+	`active` tinyint(1) DEFAULT '1',
+	`completedBy` varchar(64) DEFAULT '',
+	`insertTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -131,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `logs` (
 	`pid` varchar(17) NOT NULL,
 	`name` varchar(32) NOT NULL,
 	`action` varchar(255) DEFAULT NULL,
-	`insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`insertTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -140,5 +140,6 @@ CREATE TABLE IF NOT EXISTS `persis_vars` (
 	`mayor` varchar(32) NOT NULL,
 	`tax` varchar(100) NOT NULL DEFAULT '[0,0,0]', /*atm,item,vehicle*/
 	`bank` int(100) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`threat`,`mayor`,`tax`,`bank`)
+	`announcement` varchar (140) NOT NULL DEFAULT '',
+	PRIMARY KEY (`threat`,`mayor`,`tax`,`bank`,`announcement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
