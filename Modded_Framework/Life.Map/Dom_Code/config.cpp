37b3b48c0bcb930fce8f9df1f43ba23f0ea5bd0d
Dom <dom_thomas@hotmail.co.uk>
2020-04-17 20:29:40 +0100

    Relative paths

--- Modded_Framework/Life.Map/Dom_Code/config.cpp
+++ Modded_Framework/Life.Map/Dom_Code/config.cpp
@@ -1,18 +1,16 @@
-class CfgFunctions {
-	class Systems {
-		tag = "DT";
-		#include "\Dom_Code\Vehicles\config.cpp"
-		#include "\Dom_Code\Sync\config.cpp"
-		#include "\Dom_Code\Police\config.cpp"
-		#include "\Dom_Code\Phone\config.cpp"
-		#include "\Dom_Code\Shops\config.cpp"
-		#include "\Dom_Code\Medical\config.cpp"
-		#include "\Dom_Code\Markers\config.cpp"
-		#include "\Dom_Code\Interaction\config.cpp"
-		#include "\Dom_Code\Functions\config.cpp"
-		#include "\Dom_Code\Crafting\config.cpp"
-		#include "\Dom_Code\Companies\config.cpp"
-		#include "\Dom_Code\Civilian\config.cpp"
-		#include "\Dom_Code\Buildings\config.cpp"
-	};
+class Systems {
+	tag = "DT";
+	#include "Vehicles\config.cpp"
+	#include "Sync\config.cpp"
+	#include "Police\config.cpp"
+	#include "Phone\config.cpp"
+	#include "Shops\config.cpp"
+	#include "Medical\config.cpp"
+	#include "Markers\config.cpp"
+	#include "Interaction\config.cpp"
+	#include "Functions\config.cpp"
+	#include "Crafting\config.cpp"
+	#include "Companies\config.cpp"
+	#include "Civilian\config.cpp"
+	#include "Buildings\config.cpp"
 };
